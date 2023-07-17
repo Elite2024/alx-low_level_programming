5bd63cc71a3646bcabfd15309ade9f817fb527da
Elite2024 <afoke40@gmail.com>
2023-07-17 13:04:32 -0700

    printin combination of digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,15 @@ int main(void)
 {
 	int n, m;
 
-	for (n = 48; n <= 56; m++)
+	for (n = 48; n <= 56; n++)
 	{
-		for (n = 49; m <= 57; m++)
+		for (m = 49; m <= 57; m++)
 		{
 			if (m > n)
 			{
 				putchar(n);
 				putchar(m);
-				if (m != 56 || m != 57)
+				if (n != 56 || m != 57)
 				{
 					putchar(' ');
 					putchar(',');
