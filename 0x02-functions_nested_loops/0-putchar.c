2c218f69838d0aaa95ab7f07e7aba432f8ed1301
Elite2024 <afoke40@gmail.com>
2023-07-18 15:31:55 -0700

    working on functions and nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - This is the entry point of this program.
+ * Description: This program will prints _putchar followed by a new line.
+ * return: 0 (success)
+ */
+int main(void)
+{
+putchar('-');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+This projects involve writing some basic codes in C.
+Specifically on functions, nested loop.
