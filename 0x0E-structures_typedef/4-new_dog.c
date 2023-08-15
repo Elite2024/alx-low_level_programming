221d4f4466533cec6240e5743f98154902b4baf0
Elite2024 <afoke40@gmail.com>
2023-08-15 12:37:29 -0700

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,29 +13,29 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog_ptr;
+	dog_t *new_dog_ptr;
 
-    /* Allocate memory for the new dog structure */
-    new_dog_ptr = malloc(sizeof(dog_t));
-    if (new_dog_ptr == NULL)
-        return NULL;
+	/* Allocate memory for the new dog structure */
+	new_dog_ptr = malloc(sizeof(dog_t));
+	if (new_dog_ptr == NULL)
+		return (NULL);
 
-    /* Allocate memory for copying and storing the name */
-    new_dog_ptr->name = strdup(name);
-    if (new_dog_ptr->name == NULL) {
-        free(new_dog_ptr);
-        return NULL;
-    }
+	/* Allocate memory for copying and storing the name */
+	new_dog_ptr->name = strdup(name);
+	if (new_dog_ptr->name == NULL)
+	{
+		free(new_dog_ptr);
+		return (NULL);
+	}
 
-    /* Allocate memory for copying and storing the owner */
-    new_dog_ptr->owner = strdup(owner);
-    if (new_dog_ptr->owner == NULL) {
-        free(new_dog_ptr->name);
-        free(new_dog_ptr);
-        return NULL;
-    }
-
-    new_dog_ptr->age = age;
-
-    return new_dog_ptr;
+	/* Allocate memory for copying and storing the owner */
+	new_dog_ptr->owner = strdup(owner);
+	if (new_dog_ptr->owner == NULL)
+	{
+		free(new_dog_ptr->name);
+		free(new_dog_ptr);
+		return (NULL);
+	}
+	new_dog_ptr->age = age;
+	return (new_dog_ptr);
 }
