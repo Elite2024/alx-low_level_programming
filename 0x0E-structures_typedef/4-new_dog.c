311399f8b7c55262f92646f277ef80eabedfad92
Elite2024 <afoke40@gmail.com>
2023-08-15 12:13:56 -0700

    A door is what a dog is perpetually on the wrong side of

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * new_dog - Create a new dog.
+ * @name: Pointer to the name of the dog.
+ * @age: Age of the dog.
+ * @owner: Pointer to the owner's name of the dog.
+ *
+ * Return: Pointer to the new dog structure, or NULL on failure.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+    dog_t *new_dog_ptr;
+
+    /* Allocate memory for the new dog structure */
+    new_dog_ptr = malloc(sizeof(dog_t));
+    if (new_dog_ptr == NULL)
+        return NULL;
+
+    /* Allocate memory for copying and storing the name */
+    new_dog_ptr->name = strdup(name);
+    if (new_dog_ptr->name == NULL) {
+        free(new_dog_ptr);
+        return NULL;
+    }
+
+    /* Allocate memory for copying and storing the owner */
+    new_dog_ptr->owner = strdup(owner);
+    if (new_dog_ptr->owner == NULL) {
+        free(new_dog_ptr->name);
+        free(new_dog_ptr);
+        return NULL;
+    }
+
+    new_dog_ptr->age = age;
+
+    return new_dog_ptr;
+}
